eb2031abcd78c9422e985b9f2ff76682fd8ea0ea
Tom Kusak <tkusak@nd.edu>
2024-06-17 17:36:38 -0500

    stuff working now

--- app/scripts/read-df.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("../data/predraft_rankings.csv")
-
-print(df)
---
+++ app/scripts/read_df.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def read_df():
+    df = pd.read_csv("app/data/predraft_rankings.csv")
+    return df
+
